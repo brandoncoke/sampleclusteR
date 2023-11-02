27acab6c9035a10d07a5b6f71f3bf75cb7be4462
Brandon <65897011+brandoncoke@users.noreply.github.com>
2023-11-02 16:05:51 +0000

    Update get.clusters.as.frame.R

--- R/get.clusters.as.frame.R
+++ R/get.clusters.as.frame.R
@@ -4,14 +4,23 @@ get.clusters.as.frame= function(cluster_list){
   cluster_list_lengths_max= cluster_list_lengths[which.max(cluster_list_lengths)[1]] #identifies length of cluster title with longest title
   cluster_frame= NULL
   #cbind the clusters sample names- fill with NA by selecting an indice range outide of length
-  for(i in 1:length(cluster_list)){
-    a_cluster_list= as.data.frame(cluster_list[i])
-    a_cluster_list= a_cluster_list[,1]
-    a_cluster_list= a_cluster_list[1:cluster_list_lengths_max]
-    a_cluster_list= as.character(a_cluster_list)
-    cluster_frame=cbind(cluster_frame,a_cluster_list)
-    colnames(cluster_frame)[i]= paste0("Cluster_",i)
+  #issue with output not dataframe with only 1 sample per cluster
+  if(cluster_list_lengths_max > 1){
+    for(i in 1:length(cluster_list)){
+      a_cluster_list= as.data.frame(cluster_list[i])
+      a_cluster_list= a_cluster_list[,1]
+      a_cluster_list= a_cluster_list[1:cluster_list_lengths_max]
+      a_cluster_list= as.character(a_cluster_list)
+      cluster_frame=cbind(cluster_frame,a_cluster_list)
+      colnames(cluster_frame)[i]= paste0("Cluster_",i)
+    }
+  }else{
+    cluster_frame= t(data.frame(unlist(cluster_list)))
+    cluster_frame= rbind(cluster_frame, cluster_frame)
+    row.names(cluster_frame)= 1:2
+    colnames(cluster_frame)= paste0("Cluster_",1:ncol(cluster_frame))
   }
+
   output= as.data.frame(cluster_frame)
   output= as.data.frame(apply(output, 2, function(x){
     gsub("[(|)]", "_", x)})) #as parenthesis treated as regex... get rid of.
