18954d649ee4d6329b9025e6e6dc05f3b0c675b3
Brandon <65897011+brandoncoke@users.noreply.github.com>
2023-11-03 12:50:41 +0000

    Add files via upload

--- R/get.min.diss.r
+++ R/get.min.diss.r
@@ -2,7 +2,7 @@ get.min.diss= function(a_table){
   require(cluster)
   a_table[is.na(a_table)]= "a" #resolves issues with NA; cluster::daisy does not work with NAs
   a_table= data.matrix(a_table)
-  diss.mat <- data.matrix(cluster::daisy(a_table))
+  diss.mat <- data.matrix(cluster::daisy(a_table, warnBin=F))
   min.dis <- lapply(1:nrow(diss.mat), function(X){
     return(unlist(which(diss.mat[X,] == min(diss.mat[X,]))))
   })
