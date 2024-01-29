80108c21e2411076cc5c8ab2ba27038c963bd477
Brandon <65897011+brandoncoke@users.noreply.github.com>
2024-01-29 16:43:37 +0000

    minor combination fix

--- R/get.group.combs.R
+++ R/get.group.combs.R
@@ -4,11 +4,12 @@ get.group.combs= function(groups){ #setting up all possible group combinations e
   group_levels= group_levels[group_levels != "X"]
   output= NULL
   i=1
-  while(i < length(group_levels)){ #cleaner- all permutations for comparison. Recursion no faster in R... 
-    #paste every combo up to the length of the levels until i is the last group which cant have any combos without repeating or doing a self comparison
+  total_levels= length(group_levels)
+  while(i < total_levels){
     output= c(output,
-      paste0(group_levels[i], group_levels[(i+1):length(group_levels)]))
-    i=i+1
-  }
+              paste0(group_levels[i], ":", group_levels[(i+1):total_levels])
+              )
+    i= i+1
+    }
   return(output)
   }
--- R/set.exp.groups.R
+++ R/set.exp.groups.R
@@ -3,8 +3,8 @@ set.exp.groups= function(groups){
   combinations= get.group.combs(groups)
   lapply(combinations,function(X){
     exp_design= rep("X",length(groups))
-    first_set= substr(X,1,1) #grabs symbols representing two groups
-    second_set= substr(X,2,2)
+    first_set= as.integer(unlist(strsplit(X, ":")))[1]
+    second_set= as.integer(unlist(strsplit(X, ":")))[2]
     exp_design[groups == first_set]=0 #then assigns them on the exp_design
     exp_design[groups == second_set]=1
     paste(exp_design,collapse= "") #needed for setup.comparisons
