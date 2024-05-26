cbd83a5f2c6e9aaf0eb8e385c7bf9c6f66f01d30
Brandon <65897011+brandoncoke@users.noreply.github.com>
2024-05-26 10:42:53 +0100

    dsf

--- R/assign.group.R
+++ R/assign.group.R
@@ -1,7 +1,7 @@
 #assigns groups numbers
-assign.group= function(assoc_list, clusterframe){
+assign.group= function(assoc_list, sample_names){
   group= NA
-  for(i in 1:length(clusterframe)){ #will slow the code down but need to check all lists.
+  for(i in 1:length(sample_names)){ #will slow the code down but need to check all lists.
     if(assoc_list %in% clusterframe[[i]]){
       group= i
     }
