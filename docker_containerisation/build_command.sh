1d60d8afcd57c2800da7fa56f5fc039c67487c08
Brandon <65897011+brandoncoke@users.noreply.github.com>
2025-03-21 14:08:56 +0000

    update to install scripts

--- docker_containerisation/build_command.sh
+++ docker_containerisation/build_command.sh
@@ -1,12 +1,12 @@
 #/bin/sh
-cd docker_containerisation
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
-#docker create sampleclusteR
-#sudo docker container ls -a
-#net stop winnat
-#sudo docker start sampleclusteR
-#net start winnat
-sudo  docker build -o - . --tag sampleclusteR
-sudo docker create sampleclusteR
-sudo docker export --output="sampleclusteR_docker_container.tar.gz" sampleclusteR # create the docker image as a tar
+docker stop $(sudo docker ps -a -q)
+docker rm $(sudo docker ps -a -q)
+docker container ls -a
+net stop winnat
+net start winnat
+#sudo docker create samplecluster
+docker build -t samplecluster . #cannot name image with upper case- hence all lower
+docker save --output="GEOpythia_docker_container.tar.gz" samplecluster
+#Below loads the samplecluster image and then runs it with local directory- loads with temp_acc user.
+docker load < GEOpythia_docker_container.tar.gz
+docker run -ti -v /home:/home samplecluster R
