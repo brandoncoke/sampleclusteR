9673edf98df30f30ac698720589be6e131c9ab9e
Brandon <65897011+brandoncoke@users.noreply.github.com>
2025-03-24 15:00:37 +0000

    Update assign.group.R

--- R/assign.group.R
+++ R/assign.group.R
@@ -1,9 +1,12 @@
 #assigns groups numbers
-assign.group= function(assoc_list, sample_names){
+assign.group= function(sample_name, assoc_list){
   group= NA
-  for(i in 1:length(sample_names)){ #will slow the code down but need to check all lists.
-    if(assoc_list %in% clusterframe[[i]]){
+  for(i in 1:length(assoc_list)){ #will slow the code down but need to check all lists. Then break
+    if(sample_name %in%
+       unlist(assoc_list[i])
+      ){
       group= i
+      break
     }
   }
   return(group)
